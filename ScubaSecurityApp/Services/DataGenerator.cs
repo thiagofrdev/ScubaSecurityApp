29a7c9424624cfc9f415a277315e152de9618ed2
Thiago Ferreira Ribeiro <thiago26ribeiro@gmail.com>
2026-04-16 08:24:04 -0300

    feat: adiciona o gerador de dados para os mergulhadores

---
+++ ScubaSecurityApp/Services/DataGenerator.cs
@@ -0,0 +1,23 @@
+﻿using ScubaSecurityApp.Models;
+
+namespace ScubaSecurityApp.Services
+{
+    public static class DataGenerator
+    {
+        private static readonly Random _random = new Random();
+
+        public static List<Mergulhador> GerarDadosMergulhadores(int qtd)
+        {
+            var listaDeDados = new List<Mergulhador>();
+
+            for (int id = 1; id <= qtd; id++)
+            {
+                double profundidade = _random.NextDouble() * 40; //De 0 a 40 metros
+                double pressao = 50 + (_random.NextDouble() * 250); //De 50 a 500 Bar
+                listaDeDados.Add(new Mergulhador(id, profundidade, pressao));
+            }
+
+            return listaDeDados;
+        }
+    }
+}
