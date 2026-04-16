5584b05e0df20fef79057dacadd2f599d805750a
Thiago Ferreira Ribeiro <thiago26ribeiro@gmail.com>
2026-04-16 08:04:32 -0300

    feat: cria a classe para representar o mergulhador

--- ScubaSecurityApp/Models/Mergulhador.cs
+++ ScubaSecurityApp/Models/Mergulhador.cs
@@ -4,7 +4,22 @@ using System.Text;
 
 namespace ScubaSecurityApp.Models
 {
-    internal class Mergulhador
+    public class Mergulhador
     {
+        public int Id { get; set; }
+        public double Profundidade { get; set; }
+        public double PressaoCilindro { get; set; }
+
+        public Mergulhador(int id, double profundidade, double pressaoCilindro)
+        {
+            Id = id;
+            Profundidade = profundidade;
+            PressaoCilindro = pressaoCilindro;
+        }
+
+        public override string ToString()
+        {
+            return $"Mergulhador ID: {Id.ToString().PadLeft(2, '0')} | Profundidade: {Profundidade,5:F1}m | Pressão: {PressaoCilindro,5:F1} Bar";
+        }
     }
 }
