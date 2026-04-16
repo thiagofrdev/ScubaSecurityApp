1d3e93048594f2ea4e77bfa06f33546f0e293464
Thiago Ferreira Ribeiro <thiago26ribeiro@gmail.com>
2026-04-16 07:55:23 -0300

    feat: cria explicitamente a classe Main

--- ScubaSecurityApp/Program.cs
+++ ScubaSecurityApp/Program.cs
@@ -1,1 +1,13 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using System;
+using ScubaSecurityApp.Models;
+
+namespace ScubaSecurityApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("=== Sistema de Monitoramento de Mergulho ===");
+        }
+    }
+}
