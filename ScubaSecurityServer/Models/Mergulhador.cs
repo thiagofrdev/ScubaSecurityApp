9d20ddbd583bcf3c3e1b41efd70b7829698b4035
Thiago Ferreira Ribeiro <thiago26ribeiro@gmail.com>
2026-06-06 14:55:29 -0300

    feat: adiciona classes base no projeto do Service

---
+++ ScubaSecurityServer/Models/Mergulhador.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ScubaSecurityServer.Models
+{
+    public class Mergulhador
+    {
+        public int Id { get; set; }
+        public double Profundidade { get; set; }
+        public double PressaoCilindro { get; set; }
+
+        public Mergulhador(int id, double profundidade, double pressaoCilindro)
+        {
+            Id = id;
+            Profundidade = profundidade;
+            PressaoCilindro = pressaoCilindro;
+        }
+
+        public override string ToString()
+        {
+            return $"Mergulhador ID: {Id.ToString().PadLeft(2, '0')} | Profundidade: {Profundidade,5:F1}m | Pressão: {PressaoCilindro,5:F1} Bar";
+        }
+    }
+}
