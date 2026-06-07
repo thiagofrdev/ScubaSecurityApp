e2a1c4771ccc462917179c4e18463cade64a4013
Thiago Ferreira Ribeiro <thiago26ribeiro@gmail.com>
2026-06-06 22:20:14 -0300

    fix: aplica correções de namespaces nas classes do sistema

--- Versao2/ScubaSecurityClient/Algorithms/Sorters.cs
+++ Versao2/ScubaSecurityClient/Algorithms/Sorters.cs
@@ -1,8 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
-using ScubaSecurityServer.Models;
+using ScubaSecurityClient.Models;
 
-namespace ScubaSecurityServer.Algorithms
+namespace ScubaSecurityClient.Algorithms
 {
     public static class Sorters
     {
